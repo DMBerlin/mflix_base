44010abd5ab1c62631b0da2ad7e7cdc05d58f872
Daniel Marinho <danielmarinho@outlook.com>
2020-04-06 17:01:18 -0300

    Changed Setup from Object to a Singleton

--- src/config/setup.ts
+++ src/config/setup.ts
@@ -1,11 +1,20 @@
 const functions = require('firebase-functions')
 
-function build () {
-  return {
-    MONGO_URI: process.env.MONGODB_URI || functions.config().mongodb.uri,
-    MONGO_DBNAME: process.env.MONGODB_NAME || functions.config().mongodb.name,
-    MONGO_COLLECTION: process.env.MONGODB_COLLECTION || functions.config().mongodb.collection
+class Setup {
+
+  private static singleton: Setup
+
+  private constructor (public MONGO_URI: string, public MONGO_DBNAME: string, public MONGO_COLLECTION: string) {}
+
+  static build (MONGO_URI: string, MONGO_DBNAME: string, MONGO_COLLECTION: string) {
+    if (!Setup.singleton) Setup.singleton = new Setup(MONGO_URI, MONGO_DBNAME, MONGO_COLLECTION)
+    return Setup.singleton
   }
+
 }
 
-module.exports = build()
+module.exports = Setup.build(
+  process.env.MONGODB_URI || functions.config().mongodb.uri,
+  process.env.MONGODB_NAME || functions.config().mongodb.name,
+  process.env.MONGODB_COLLECTION || functions.config().mongodb.collection
+)
